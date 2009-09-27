cafeb0fafd8a4c00f7eda804656ac7fa3c0d2ed2
looloobs <laurenrothlisberger@Macintosh.local>
2009-09-27 13:09:20 -0500

    fix changeunitinusers again

--- db/migrate/20090825203819_change_unit_in_users.rb
+++ db/migrate/20090825203819_change_unit_in_users.rb
@@ -1,10 +1,10 @@
 class ChangeUnitInUsers < ActiveRecord::Migration
   def self.up
-    remove_column :users, :unit
-    add_column :users, :unit, :integer
+    change_column :users, :unit, :integer
   end
 
   def self.down
+
     remove_column :users, :unit
   end
 end
