d5dce68ffa0c9f8558529f1553dc85b3b493f9dc
looloobs <laurenrothlisberger@Macintosh.local>
2009-09-27 19:41:28 -0500

    changed migration sms

--- db/migrate/20090904164900_add_sms_to_soldiers.rb
+++ db/migrate/20090904164900_add_sms_to_soldiers.rb
@@ -1,10 +1,10 @@
 class AddSmsToSoldiers < ActiveRecord::Migration
   def self.up
     remove_column :soldiers, :nokaddress
-    add_column :soldiers, :sms, :boolean
+    add_column :soldiers, :sms, :string
   end
 
   def self.down
-    remove_column :soldiers, :lastname
+    add_column :soldiers, :nokaddress
   end
 end
