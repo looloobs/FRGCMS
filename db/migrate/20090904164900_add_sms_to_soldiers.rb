7d7fe8d10570f590a45e506ee67fde6b77c3a326
looloobs <laurenrothlisberger@Macintosh.local>
2009-09-28 13:12:48 -0500

    changes to session_controller

--- db/migrate/20090904164900_add_sms_to_soldiers.rb
+++ db/migrate/20090904164900_add_sms_to_soldiers.rb
@@ -1,6 +1,7 @@
 class AddSmsToSoldiers < ActiveRecord::Migration
   def self.up
-    
+    remove_column :soldiers, :nokaddress
+     add_column :soldiers, :nokaddress, :string
   end
 
   def self.down
