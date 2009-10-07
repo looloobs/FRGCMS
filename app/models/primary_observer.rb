89da42733e36beaa461136b09bbc9d933c15c8a1
looloobs <laurenrothlisberger@Macintosh.local>
2009-10-07 09:25:50 -0500

    primary changes

--- app/models/primary_observer.rb
+++ app/models/primary_observer.rb
@@ -2,8 +2,8 @@ class PrimaryObserver < ActiveRecord::Observer
 
     def after_create(primary)
         PrimaryMailer.deliver_welcome_primary_email(primary) if primary.email?
-        if primary.relationship == "Spouse"
+        /if primary.relationship == "Spouse"
           PrimaryMailer.deliver_welcome_frg_email(primary)
-        end
+        end/
     end
 end
