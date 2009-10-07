c48aa7f7764e4b377b892faba46c32d9ee33c713
looloobs <laurenrothlisberger@Macintosh.local>
2009-10-07 09:16:21 -0500

    primary fix mailer

--- app/models/primary_mailer.rb
+++ app/models/primary_mailer.rb
@@ -12,8 +12,8 @@ class PrimaryMailer < ActionMailer::Base
   def welcome_frg_email(primary)  
     recipients primary.company.users.find_by_position('FRG Leader').email
     from "frgcms@gmail.com"
-    reply_to soldier.company.users.find_by_position('FRG Leader').email  
-    subject "Family Readiness Group for " .concat(soldier.rank).concat(" ").concat(soldier.firstname).concat(" ").concat(soldier.lastname)
+    reply_to primary.company.users.find_by_position('FRG Leader').email  
+    subject "New Spouse Added for " .concat(primary.soldier.rank).concat(" ").concat(primary.soldier.firstname).concat(" ").concat(primary.soldier.lastname)
     sent_on Time.now 
     body :primary => primary, :soldier => primary.soldier
   end
