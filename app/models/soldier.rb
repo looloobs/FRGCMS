f3345c234664526dfacdb5159af3103c5310511f
looloobs <laurenrothlisberger@Macintosh.local>
2009-10-12 22:41:56 -0500

    add validations

--- app/models/soldier.rb
+++ app/models/soldier.rb
@@ -7,5 +7,7 @@ class Soldier < ActiveRecord::Base
   belongs_to :user
   accepts_nested_attributes_for :primaries, :kids, :additionals
   validates_presence_of :rank,:firstname, :lastname, :maritalstatus, :birth_date, :address,:city,:state, :zip, :email, :cellphone, 
+  :on => :create, :on => :create, :message => "can't be blank"
+  validates_presence_of :rank,:firstname, :lastname, :maritalstatus, :birth_date, :address,:city,:state, :zip, :email, :cellphone, 
   :on => :create, :on => :update, :message => "can't be blank" 
 end
