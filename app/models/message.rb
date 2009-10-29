4f4e32cd12dbcc5186e5784ee94d893041b29c09
looloobs <laurenrothlisberger@Macintosh.local>
2009-10-28 22:03:44 -0400

    fix message titles

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
   belongs_to :users
-  #validates_presence_of :bcc_email, :on => :create, :message => "can't be blank"
+  validates_presence_of :bcc_email, :on => :create, :message => "can't be blank"
   
   
 
