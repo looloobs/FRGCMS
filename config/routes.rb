d34c84df8a2c0f4aefab0b017260f315ece1deb0
looloobs <laurenrothlisberger@Macintosh.local>
2010-01-05 10:58:10 -0600

    videos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :videos
+
   map.resources :notes
 
   map.resources :users_messages
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091231024956) do
+ActiveRecord::Schema.define(:version => 20100105022746) do
 
   create_table "additionals", :force => true do |t|
     t.string   "relationship"
@@ -247,6 +247,14 @@ ActiveRecord::Schema.define(:version => 20091231024956) do
     t.integer  "message_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "videos", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "title"
   end
 
 end
