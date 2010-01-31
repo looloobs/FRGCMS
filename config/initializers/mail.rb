9395072d4dfed2721912efd4540d1019ddc340bf
looloobs <laurenrothlisberger@Macintosh.local>
2010-01-31 13:10:11 -0600

    mail server

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,7 +2,7 @@
 #require 'smtp_tls'
 
 ActionMailer::Base.smtp_settings = {
-  :address => "mail-gw.frgcms.com",
+  :address => "mail.blueboxgroup.com",
   :port => 2500,
   :authentication => :login,
   :user_name => "admin@frgcms.com",
