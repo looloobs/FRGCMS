72d6f785ac57fb0b2b55ed461e2698fe79a32dfb
looloobs <laurenrothlisberger@Macintosh.local>
2010-01-31 21:57:03 -0600

    remove migration

--- db/migrate/20100131232746_remove_active_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveActiveFromUsers < ActiveRecord::Migration
-  def self.up
-    remove_column :users, :active, :string
-  end
-
-  def self.down
-    add_column :users, :active
-  end
-end
