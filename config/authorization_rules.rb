3038d984d508d9ef19964606f04a3e4add14ef5d
looloobs <laurenrothlisberger@Macintosh.local>
2010-03-01 15:25:11 -0600

    fix authorization rules

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,12 +1,13 @@
 authorization do
 
   role :admin do
-    has_permission_on [:battalions, :companies, :messages], :to => [:index, :show, :new, :create, :edit, :update, :destroy] 
+    has_permission_on [:battalions, :companies, :platoons, :messages], :to => [:index, :show, :new, :create, :edit, :update, :destroy] 
   end
   
   role :battalion do
     has_permission_on [:companies], :to => [:new, :create, :show, :edit, :update]
     has_permission_on [:messages], :to => [:new, :create, :index, :show, :edit, :update]
+    has_permission_on [:platoons], :to => [:show, :edit, :update, :new, :create, :destroy]
     has_permission_on [:battalions], :to => [:show, :edit, :update] do
       if_attribute :id => is {user.battalion.id}
     end
