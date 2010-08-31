ff361c2bc2704048d01b3b7a932a62a5cc16fe57
looloobs <laurenrothlisberger@Macintosh.local>
2010-08-30 21:43:34 -0500

    add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ config.action_mailer.raise_delivery_errors = true
 config.action_mailer.perform_deliveries = true
 config.action_mailer.delivery_method = :smtp
 
+config.action_mailer.default_url_options = { :host => "frgcms.com" }
 # See everything in the log (default is :info)
 # config.log_level = :debug
 
